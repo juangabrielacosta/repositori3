ae796dedc777629881e173d42484672243c24853
juan acosta <juangabrielacosta@outlook.com>
2017-11-13 15:26:08 -0300

    prueba1 modificado

--- prueba1/src/rodado/program.java
+++ prueba1/src/rodado/program.java
@@ -1,3 +1,4 @@
+
 package rodado;
 
 public class program {
@@ -28,3 +29,35 @@ mostrar (auto2);
 	}
 
 }
+=======
+package rodado;
+
+public class program {
+
+	public program() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Vehiculos moto = new motocicleta("zanella");
+//		//moto =new diesel(moto);
+//		moto = new gasoil(moto);
+//		
+		Vehiculos auto= new automovil("topo");
+		auto = new gasoil(auto);
+mostrar(auto);
+
+Vehiculos auto2 = new automovil("mercedes");
+auto2 = new diesel(auto2);
+mostrar (auto2);
+	}
+	public static void mostrar (Vehiculos v) {
+		System.out.println(v.descripcion());
+		System.out.println(v.velocidadMaxima());
+		System.out.println(v.Consumo());
+		
+	}
+
+}
+
